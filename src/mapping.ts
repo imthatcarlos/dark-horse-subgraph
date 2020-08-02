67d187684f17c858e1ca47d5755cdaddc1f03743
Carlos Beltran <imthatcarlos@gmail.com>
2020-08-02 14:51:44 -0400

    update schema + mappings

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,74 +1,102 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
 import {
-  Contract,
   CampaignCompleted,
   CampaignCreated,
   UserRegistered,
   UserRewardsWithdrawn
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/MobilityCampaigns/MobilityCampaigns"
+import {
+  MobilityCampaign,
+  RewardOwner,
+  MobilityCampaignOwner,
+  Reward
+} from "../generated/schema"
 
 export function handleCampaignCompleted(event: CampaignCompleted): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.creator = event.params.creator
-  entity.totalCampaignReceivers = event.params.totalCampaignReceivers
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.EXPIRES_IN_SECONDS(...)
-  // - contract.MIN_REWARDS_WITHDRAW_WEI(...)
-  // - contract.activeCampaignOwners(...)
-  // - contract.campaignReceivers(...)
-  // - contract.dataProviders(...)
-  // - contract.graphIndexer(...)
-  // - contract.rewardsWithdrawnWei(...)
-  // - contract.totalCampaignReceivers(...)
-  // - contract.totalDataProviders(...)
-  // - contract.totalRewardsWei(...)
-  // - contract.getReceiveCampaign(...)
-  // - contract.getProvideData(...)
-  // - contract.getActiveCampaignId(...)
-  // - contract.getActiveCampaign(...)
-  // - contract.calculateRefundedWei(...)
-  // - contract.getActiveCampaignIdsUsers(...)
-  // - contract.getActiveCampaignUsers(...)
-  // - contract.getIsCampaignActive(...)
+  const entity = new MobilityCampaign(event.params.creator.toHex() + "-" + event.params.idx.toString());
+  entity.closedAtCampaignReceivers = event.params.totalCampaignReceivers;
+  entity.isActive = false;
+  entity.save();
 }
 
-export function handleCampaignCreated(event: CampaignCreated): void {}
+export function handleCampaignCreated(event: CampaignCreated): void {
+  const entity = new MobilityCampaign(event.params.creator.toHex() + "-" + event.params.idx.toString());
 
-export function handleUserRegistered(event: UserRegistered): void {}
+  entity.idx = event.params.idx;
+  entity.creator = event.params.creator.toHex();
+  entity.organization = event.params.organization.toString();
+  entity.category = event.params.category;
+  entity.title = event.params.title;
+  entity.createdAt = event.params.createdAt;
+  entity.isActive = true;
 
-export function handleUserRewardsWithdrawn(event: UserRewardsWithdrawn): void {}
+  entity.save();
+
+  // create an entity for MobilityCampaignOwner if it doesn't exist
+  let campaignOwner = MobilityCampaignOwner.load(event.transaction.from.toHex());
+  if (campaignOwner == null) {
+    campaignOwner = new MobilityCampaignOwner(event.transaction.from.toHex());
+    campaignOwner.owner = event.params.creator;
+    campaignOwner.totalContributedWei = event.params.budgetWei;
+  } else {
+    campaignOwner.totalContributedWei = campaignOwner.totalContributedWei + event.params.budgetWei;
+  }
+
+  campaignOwner.save();
+}
+
+export function handleUserRegistered(event: UserRegistered): void {
+  const entity = new RewardOwner(event.transaction.from.toHex());
+
+  entity.owner = event.params.account;
+  entity.enabledAt = event.params.enabledAt;
+  entity.enabledAtCampaignIdx = event.params.enabledAtCampaignIdx;
+  entity.save();
+}
+
+export function handleUserRewardsWithdrawn(event: UserRewardsWithdrawn): void {
+  const entity = new RewardOwner(event.transaction.from.toHex());
+
+  entity.totalRewardsWei = event.params.totalRewardsWei;
+  entity.save();
+
+  const logEntity = new Reward(event.transaction.from.toHex() + "-" + event.logIndex.toString());
+  logEntity.owner = event.params.account.toHex();
+  logEntity.rewardsWei = event.params.rewardsWei;
+  logEntity.withdrewAt = event.params.withdrewAt;
+  logEntity.save();
+}
+
+// Note: If a handler doesn't require existing field values, it is faster
+// _not_ to load the entity from the store. Instead, create it fresh with
+// `new Entity(...)`, set the fields that should be updated and save the
+// entity back to the store. Fields that were not set or unset remain
+// unchanged, allowing for partial updates to be applied.
+
+// It is also possible to access smart contracts from mappings. For
+// example, the contract that has emitted the event can be connected to
+// with:
+//
+// let contract = Contract.bind(event.address)
+//
+// The following functions can then be called on this contract to access
+// state variables and other data:
+//
+// - contract.EXPIRES_IN_SECONDS(...)
+// - contract.MIN_REWARDS_WITHDRAW_WEI(...)
+// - contract.activeCampaignOwners(...)
+// - contract.campaignReceivers(...)
+// - contract.dataProviders(...)
+// - contract.graphIndexer(...)
+// - contract.rewardsWithdrawnWei(...)
+// - contract.totalCampaignReceivers(...)
+// - contract.totalDataProviders(...)
+// - contract.totalRewardsWei(...)
+// - contract.getReceiveCampaign(...)
+// - contract.getProvideData(...)
+// - contract.getActiveCampaignId(...)
+// - contract.getActiveCampaign(...)
+// - contract.calculateRefundedWei(...)
+// - contract.getActiveCampaignIdsUsers(...)
+// - contract.getActiveCampaignUsers(...)
+// - contract.getCampaignsIndexer(...)
